e6c8ce1fd5839c96404028abeb658ff59bf2d36f
Peter_Narouz <peterrefaat78@Gmail.com>
2020-12-07 21:12:51 +0200

    Add files via upload

--- landing-page New/js/app.js
+++ landing-page New/js/app.js
@@ -14,7 +14,7 @@
 */
 
 /**
- * Define Global Variables
+ * Define 
  * 
 */
 
@@ -36,7 +36,7 @@
 // build the nav
 
 
-// Add class 'active' to section when near top of viewport
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -48,63 +48,85 @@
  * 
 */
 
+
+
+
+
+
+//Global Variables
+const pgSections = document.querySelectorAll('.sections');
+let unList = document.querySelector('#navbar__list');
+let documentFrag = document.createDocumentFragment();
+
 // Build menu 
 
-// Scroll to section on link click
+pgSections.forEach((section) => {
+    function generate() {
+        if (n = 0, n >= pgSections.length, n++) {
+        console.log(n);
+        }
+    }
 
-// Set sections as active
+    let navName = section.getAttribute("data-nav");
+    let liItem = document.createElement('li');
+    let anchItem = document.createElement('a');
+    let txtNd = document.createTextNode(navName);
+    anchItem.appendChild(txtNd);
+    liItem.appendChild(anchItem);
+    documentFrag.appendChild(liItem);
+
+    // Scroll to section on link click
+    // Set sections as active
+    liItem.addEventListener('click', applyActiveClass);
+    
+    function applyActiveClass(e) {
+        const elems = document.querySelectorAll(".active-class");
+        [].forEach.call(elems, function (el) {
+            if (el.classList.contains('active-class')) {
+                el.classList.remove("active-class");
+                //el.classList.add("menu__link");
+        }});
+        e.target.className = "menu__link active-class";
+
+        section.scrollIntoView({ behavior: "smooth", block: "start" });
+        const sections = document.querySelectorAll(".your-active-class");
+        [].forEach.call(sections, function (sec) {
+            sec.classList.remove("your-active-class");
+        });
+        section.classList.add("your-active-class")
+    }
+})
+unList.appendChild(documentFrag)
 
 
-
-
-function applyActiveClass(e) {
-    const elems = document.querySelectorAll(".active-class");
-    [].forEach.call(elems, function (el) {
-        el.classList.remove("active-class");
-        el.classList.add("menu__link");
-    });
-    e.target.className = "active-class";
-
-    const section = document.getElementById(e.target.text);
-    section.scrollIntoView({ behavior: "smooth", block: "start" });
-    const sections = document.querySelectorAll(".your-active-class");
-    [].forEach.call(sections, function (sec) {
-        sec.classList.remove("your-active-class");
-    });
-    section.classList.add("your-active-class")
-}
-
-
-
+// Add class 'active' to section when near top of viewport
 window.addEventListener('scroll', () => {
-    const pageSections = document.querySelectorAll('section')
-    pageSections.forEach((section) => {
+    pgSections.forEach((section) => {
         let rect = section.getBoundingClientRect();
 
         if (rect.top >= 0 && rect.top < 150) {
-            pageSections.forEach((remover) => {
+            pgSections.forEach((remover) => {
                 remover.classList.remove('your-active-class');
             });
             section.classList.add('your-active-class');
-            const navLinks = document.querySelectorAll("a");
-            if (section.id) {
-                [].forEach.call(navLinks, function (a) {
-                    if (a.text === section.id) {
-                        a.classList.add("active-class");
-                    } else {
-                        a.classList.remove("active-class");
-                        a.classList.add("menu__link");
-                    }
-                });
-            }
+            // const navLinks = document.querySelectorAll("a");
+            // if (section.id) {
+            //     [].forEach.call(navLinks, function (a) {
+            //         if (a.text === section.id) {
+            //             a.classList.add("active-class");
+            //         } else {
+            //             a.classList.remove("active-class");
+            //             //a.classList.add("menu__link");
+            //         }
+            //    });
+           // }
         }
 
     })
 
 })
 
-
-
+//Mobile Compitability Function
 function applytoggleNav() {
     const bars = document.getElementById('menu-bars')
     bars.classList.toggle('change1');
